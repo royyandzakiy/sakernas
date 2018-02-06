b837e91de3e086eb1edf50ddde0303a76c9eee09
royyandzakiy <royyan.abdul@gmail.com>
2018-02-07 06:46:51 +0700

    try jquery-json table

--- src/entri_p.js
+++ src/entri_p.js
@@ -84,6 +84,105 @@ $.ajax(settings).done(function (dataku) {
 
 // ----MAIN APP
 class Entri_p extends Component {
+
+submit: function (e){
+  var self
+
+  e.preventDefault()
+  self = this
+
+  console.log(this.state);
+
+  var data = {
+      kode_prov: this.state.kode_prov,
+      kode_kab: this.state.kode_kab,
+      kode_kec: this.state.kode_kec,
+      kode_desa: this.state.kode_desa,
+      nks: this.state.nks,
+      semester: this.state.semester,
+      nort: this.state.nort,
+      id_user: this.state.id_user,
+      status_dok: this.state.status_dok,
+      b3_r1a: this.state.b3_r1a,
+      b3_r2a_tl: this.state.b3_r2a_tl,
+      b3_r2a_bl: this.state.b3_r2a_bl,
+      b3_r2a_th: this.state.b3_r2a_th,
+      b3_r1b: this.state.b3_r1b,
+      b3_r2b_tl: this.state.b3_r2b_tl,
+      b3_r2b_bl: this.state.b3_r2b_bl,
+      b3_r2b_th: this.state.b3_r2b_th,
+      b4_rcat: this.state.b4_rcat,
+      sls: this.state.sls,
+      nbf: this.state.nbf,
+      nbs: this.state.nbs,
+      nama_krt: this.state.nama_krt,
+      alamat: this.state.alamat,
+      keberadaan_rt: this.state.keberadaan_rt,
+      nurt: this.state.nurt,
+      jml_art: this.state.jml_art,
+    }
+
+  $.ajax({
+    type: 'POST',
+    dataType: 'json',
+    url: 'http://localhost:8002/pemutakhiran',
+  })
+  .done(function(response) {
+  console.log(response);
+
+  // This is where I have the JSON response .How can I create a dynamic table from this response **strong text**and render in on UI.  //
+  setf.setState({
+    kode_prov: response.kode_prov,
+    kode_kab: response.kode_kab,
+    kode_kec: response.kode_kec,
+    kode_desa: response.kode_desa,
+    nks: response.nks,
+    semester: response.semester,
+    nort: response.nort,
+    id_user: response.id_user,
+    status_dok: response.status_dok,
+    b3_r1a: response.b3_r1a,
+    b3_r2a_tl: response.b3_r2a_tl,
+    b3_r2a_bl: response.b3_r2a_bl,
+    b3_r2a_th: response.b3_r2a_th,
+    b3_r1b: response.b3_r1b,
+    b3_r2b_tl: response.b3_r2b_tl,
+    b3_r2b_bl: response.b3_r2b_bl,
+    b3_r2b_th: response.b3_r2b_th,
+    b4_rcat: response.b4_rcat,
+    sls: response.sls,
+    nbf: response.nbf,
+    nbs: response.nbs,
+    nama_krt: response.nama_krt,
+    alamat: response.alamat,
+    keberadaan_rt: response.keberadaan_rt,
+    nurt: response.nurt,
+    jml_art: response.jml_art,
+  })
+ }
+ render() {
+   return(
+     <table>
+       <thead>
+          <tr>
+             <th>Name</th>
+             <th>ID</th>
+             <th>Task</th>
+          </tr>
+       </thead>
+       <tbody>
+       {this.state.userData.map((data, key) => {
+          return (
+          <tr key={key}>
+            <td>{data.name}</td>
+            <td>{data.id}</td>
+            <td>{data.task}</td>
+          </tr>
+          )
+       })}
+       </tbody>
+     </table>
+
   constructor(props) {
     super(props);
   }
