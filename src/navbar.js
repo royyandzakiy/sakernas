5e79f56fc7d289a939e2ad36696b978dd9e60da8
royyandzakiy <royyan.abdul@gmail.com>
2018-02-08 06:02:51 +0700

    ruta done half

--- src/navbar.js
+++ src/navbar.js
@@ -44,9 +44,13 @@ class Navbar extends Component {
               <li class="dropdown">
                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><i class="fa fa-fw fa-bell-o"></i> Entri <span class="caret"></span></a>
                 <ul class="dropdown-menu" role="menu">
-                  <li><Link to="/pemutakhiran"><i class="fa fa-fw fa-tag"></i> <span class="badge">Entri</span> Pemutakhiran<span class="sr-only">(current)</span></Link></li>
-                  <li><Link to="/dsrt"><i class="fa fa-fw fa-tag"></i> <span class="badge">Entri</span> DSRT</Link></li>
-                  <li><Link to="/ruta"><i class="fa fa-fw fa-tag"></i> <span class="badge">Entri</span> Ruta</Link></li>
+
+                  <li><Link to="/pemutakhiran"><i class="fa fa-fw fa-tag"></i> <span class="badge">Entri</span> Pemutakhiran<span class="sr-only">(current)</span></Link></li><li>
+
+                  <Link to="/dsrt"><i class="fa fa-fw fa-tag"></i> <span class="badge">Entri</span> DSRT</Link></li><li>
+
+                  <Link to="/ruta"><i class="fa fa-fw fa-tag"></i> <span class="badge">Entri</span> Ruta</Link></li>
+
                 </ul>
               </li>
 
@@ -54,29 +58,29 @@ class Navbar extends Component {
                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><i class="fa fa-fw fa-bell-o"></i>Master <span class="caret"></span></a>
                 <ul class="dropdown-menu" role="menu">
                   <li><Link to="/petugas-lap">Petugas Lapangan</Link></li>
-                  <li><Link to="/pemutakhiran">Pengguna</Link></li>
-                  <li><Link to="/pemutakhiran">Wilayah</Link></li>
-                  <li><Link to="/pemutakhiran">Wilayah Sampel</Link></li>
+                  <li><Link to="/">Pengguna</Link></li>
+                  <li><Link to="/">Wilayah</Link></li>
+                  <li><Link to="/">Wilayah Sampel</Link></li>
                 </ul>
               </li>
 
               <li class="dropdown">
                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><i class="fa fa-fw fa-bell-o"></i>Rekap <span class="caret"></span></a>
                 <ul class="dropdown-menu" role="menu">
-                  <li><Link to="/pemutakhiran">Progres Entri</Link></li>
-                  <li><Link to="/pemutakhiran">Operator</Link></li>
-                  <li><Link to="/pemutakhiran">Pengguna</Link></li>
-                  <li><Link to="/pemutakhiran">Cek Kewajaran</Link></li>
+                  <li><Link to="/">Progres Entri</Link></li>
+                  <li><Link to="/">Operator</Link></li>
+                  <li><Link to="/">Pengguna</Link></li>
+                  <li><Link to="/">Cek Kewajaran</Link></li>
                 </ul>
               </li>
 
               <li class="dropdown">
                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><i class="fa fa-fw fa-bell-o"></i>Utilitas <span class="caret"></span></a>
                 <ul class="dropdown-menu" role="menu">
-                  <li><Link to="/pemutakhiran">Revalidasi</Link></li>
-                  <li><Link to="/pemutakhiran">Setujui Data</Link></li>
-                  <li><Link to="/pemutakhiran">Weighting</Link></li>
-                  <li><Link to="/pemutakhiran">Derivasi</Link></li>
+                  <li><Link to="/">Revalidasi</Link></li>
+                  <li><Link to="/">Setujui Data</Link></li>
+                  <li><Link to="/">Weighting</Link></li>
+                  <li><Link to="/">Derivasi</Link></li>
                 </ul>
               </li>
 
