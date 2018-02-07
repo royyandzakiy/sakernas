14ab96bc93ccf19118cbf47370ed6aa20126ee76
royyandzakiy <royyan.abdul@gmail.com>
2018-02-08 01:20:35 +0700

    feature: entri-p done! :octocat:

--- src/entri_p.js
+++ src/entri_p.js
@@ -4,55 +4,22 @@ import './css/main.css';
 // table stuff
 import $ from 'jquery';
 
-// ----DATA
-//    data ini akan di load setelah tombol 'refresh' ditekan
-//    GET data berdasarkan parameter pada input box
-var pemutakhiran = [];
-pemutakhiran = [{
-      kode_prov: 99,
-      kode_kab: 9,
-      kode_kec: 99,
-      kode_desa: 99,
-      nks: 99999,
-      semester: 9,
-      nort: 9,
-      id_user: 9999999,
-      status_dok: '9',
-      //--data lain--
-      sls: 9,
-      nbf: 9,
-      nbs: 0,
-      nama_krt: '999999',
-      alamat: 'DUSUN 999999',
-      keberadaan_rt: '',
-      nurt: 9,
-      jml_art: 9
-    }]
+// ----ALMANAC FUNCTION
+function set_settings(_url) {
 
-// ---FUNCTIONS
-const cellEditProp = {
-  mode: 'dbclick',
-  blurToSave: true
-};
+    var settings = {
+      "async": true,
+      "crossDomain": true,
+      "url": _url,
+      "method": "GET",
+      "headers": {
+        "content-type": "application/x-www-form-urlencoded",
+        "cache-control": "no-cache",
+        "postman-token": "a7fad470-f7a5-ec21-2aa5-4bd2f532cd6b"
+      }
+    }
 
-// If you want to enable deleteRow, you must enable row selection also.
-const selectRowProp = {
-  mode: 'checkbox'
-};
-
-const options = {
-};
-
-var settings = {
-  "async": true,
-  "crossDomain": true,
-  "url": "http://localhost:8002/pemutakhiran",
-  "method": "GET",
-  "headers": {
-    "content-type": "application/x-www-form-urlencoded",
-    "cache-control": "no-cache",
-    "postman-token": "a7fad470-f7a5-ec21-2aa5-4bd2f532cd6b"
-  }
+    return settings;
 }
 
 // ----MAIN APP
@@ -63,49 +30,107 @@ class Entri_p extends Component {
       var data_pemutakhiran = [];
 
       $(document).ready(function(){
+          // get data: provinsi
+          $.ajax(set_settings("http://localhost:8002/master-prov")).done(function (_data) {
+              var data = _data;
 
-          $.ajax(settings).done(function (_data_pemutakhiran) {
-              data_pemutakhiran = _data_pemutakhiran;
-              console.log("data_pemutakhiran: " + JSON.stringify(data_pemutakhiran));
+                for (var i=0; i<data.length; i++) {
+                	$("#entri-p-prov").append(
+                  "<option value="+data[i]['kode_prov']+">["+data[i]['kode_prov']+"] "+data[i]['nama_prov']+"</option>"
+                  );
+                }
 
-              if (data_pemutakhiran.length == 0) {
-                $("#entri-p > tbody").append("<tr>" +
-                "List Empty" +
-                "</tr>");
-              } else {
+          });
 
-                for (var i=0; i<data_pemutakhiran.length; i++) {
-                	$("#entri-p > tbody").append(
-                  "<a href='#'><tr class='data'>"+
+          // get data: kabupaten
+          $.ajax(set_settings("http://localhost:8002/master-kab")).done(function (_data) {
+              var data = _data;
+
+                for (var i=0; i<data.length; i++) {
+                	$("#entri-p-kab").append(
+                  "<option value="+data[i]['kode_kab']+">["+data[i]['kode_kab']+"] "+data[i]['nama_kab']+"</option>"
+                  );
+                }
+
+          });
+
+      });
+
+
+  }
+
+  // function: refresh
+  refresh(e) {
+    e.preventDefault();
+
+    var entri_p_sem = $('#entri-p-sem option:selected').val();
+    var entri_p_prov = $('#entri-p-prov option:selected').val();
+    var entri_p_kab = $('#entri-p-kab option:selected').val();
+
+    $.get("http://localhost:8002/pemutakhiran",
+        {
+          entri_p_sem:entri_p_sem,
+          entri_p_prov:entri_p_prov,
+          entri_p_kab:entri_p_kab
+        },
+        function(data, status) {
+            // alert(JSON.stringify(data)); //debug
+
+            // remove rows
+            $("#entri-p > tbody > tr").remove();
+
+            // generate rows
+            if (data.length != 0)
+                for (var i=0; i<data.length; i++) {
+                  $("#entri-p > tbody").append(
+                  "<tr class='form-data' onclick=\"document.location = 'form-entri-p.html';\">"+
                     "<td>"+ (Number(i)+1).toString() +"</td><td>"+
-                    data_pemutakhiran[i]['kode_kec'] + "</td><td>" +
-                    data_pemutakhiran[i]['kode_desa'] + "</td><td>" +
-                    data_pemutakhiran[i]['nks'] + "</td><td>" +
-                    data_pemutakhiran[i]['sls'] + "</td><td>" +
-                    data_pemutakhiran[i]['status_dok'] + "</td>" +
-                  "</tr></a><tr class='edit'>"+
+                    data[i]['kode_kec'] + "</td><td>" +
+                    data[i]['kode_desa'] + "</td><td>" +
+                    data[i]['nks'] + "</td><td>" +
+                    data[i]['sls'] + "</td><td>" +
+                    data[i]['status_dok'] + "</td>" +
+                  "</tr><tr class='edit'>"+
                     "<td>"+ (Number(i)+1).toString() +"</td><td>"+
-                    "<input type=\"text\" value="+ data_pemutakhiran[i]['kode_kec'] +" />" + "</td><td>" +
-                    "<input type=\"text\" value="+ data_pemutakhiran[i]['kode_desa'] +" />" + "</td><td>" +
-                    "<input type=\"text\" value="+ data_pemutakhiran[i]['nks'] +" />" + "</td><td>" +
-                    "<input type=\"text\" value="+ data_pemutakhiran[i]['sls'] +" />" + "</td><td>" +
-                    "<input type=\"text\" value="+ data_pemutakhiran[i]['status_dok'] +" />" + "</td>" +
+                    "<input type=\"text\" value="+ data[i]['kode_kec'] +" />" + "</td><td>" +
+                    "<input type=\"text\" value="+ data[i]['kode_desa'] +" />" + "</td><td>" +
+                    "<input type=\"text\" value="+ data[i]['nks'] +" />" + "</td><td>" +
+                    "<input type=\"text\" value="+ data[i]['sls'] +" />" + "</td><td>" +
+                    "<input type=\"text\" value="+ data[i]['status_dok'] +" />" + "</td>" +
                   "</tr>");
 
                   $(".edit").css("display","none");
                   }
-              }
-          });
-      });
+            else
+                $("#entri-p > tbody").append(
+                "<tr class='data'>"+
+                  "<td colspan='6'>Tidak ada data yang sesuai</td>"+
+                "</tr>");
+        });
 
-      }
+  }
 
   render() {
     return (
       <div id="container" class="col-lg-12">
 
-      <div>
-          <input >
+      <div class="form-group col-lg-4" >
+        <label for="entri-p-sem">Semester:</label>
+        <select class="form-control" id="entri-p-sem" >
+          <option>1</option>
+          <option>2</option>
+        </select>
+
+        <label for="entri-p-prov">Provinsi:</label>
+        <select class="form-control" id="entri-p-prov">
+        </select>
+
+        <label for="entri-p-kab">Kabupaten:</label>
+        <select class="form-control" id="entri-p-kab">
+        </select>
+
+        <br />
+        <button type="button" class="btn btn-success" onClick={this.refresh}>Refresh</button>
       </div>
 
       <table id="entri-p" class="table-striped table table-bordered table-hover" >
@@ -120,6 +145,9 @@ class Entri_p extends Component {
               </tr>
           </thead>
           <tbody>
+            <tr>
+              <td colSpan='6'>Tekan Refresh</td>
+            </tr>
           </tbody>
       </table>
       </div>
